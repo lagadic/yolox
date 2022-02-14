f036dcb8c88bed6e41141e53bd5b63e6aaf673a9
jdufour <julien.dufour@inria.fr>
2022-02-14 15:46:18 +0000

    🐛 Fix voc to yolox converter

--- tools/voc_to_yolox/utils/voc.py
+++ tools/voc_to_yolox/utils/voc.py
@@ -2,6 +2,7 @@ import os
 import pickle
 import xml.etree.ElementTree as ET
 
+
 def parse_voc_annotation(ann_dir, img_dir, cache_name, labels=[]):
     if os.path.exists(cache_name):
         with open(cache_name, 'rb') as handle:
@@ -10,27 +11,28 @@ def parse_voc_annotation(ann_dir, img_dir, cache_name, labels=[]):
     else:
         all_insts = []
         seen_labels = {}
-        
+
         for ann in sorted(os.listdir(ann_dir)):
-            img = {'object':[]}
+            img = {'object': []}
 
             try:
-                tree = ET.parse(ann_dir + ann)
+                tree = ET.parse(os.path.join(ann_dir, ann))
             except Exception as e:
                 print(e)
-                print('Ignore this bad annotation: ' + ann_dir + ann)
+                print('Ignore this bad annotation: ' +
+                      os.path.join(ann_dir, ann))
                 continue
-            
+
             for elem in tree.iter():
                 if 'filename' in elem.tag:
-                    img['filename'] = img_dir + elem.text
+                    img['filename'] = os.path.join(img_dir, elem.text)
                 if 'width' in elem.tag:
                     img['width'] = int(elem.text)
                 if 'height' in elem.tag:
                     img['height'] = int(elem.text)
                 if 'object' in elem.tag or 'part' in elem.tag:
                     obj = {}
-                    
+
                     for attr in list(elem):
                         if 'name' in attr.tag:
                             obj['name'] = attr.text
@@ -39,12 +41,12 @@ def parse_voc_annotation(ann_dir, img_dir, cache_name, labels=[]):
                                 seen_labels[obj['name']] += 1
                             else:
                                 seen_labels[obj['name']] = 1
-                            
+
                             if len(labels) > 0 and obj['name'] not in labels:
                                 break
                             else:
                                 img['object'] += [obj]
-                                
+
                         if 'bndbox' in attr.tag:
                             for dim in list(attr):
                                 if 'xmin' in dim.tag:
@@ -61,6 +63,6 @@ def parse_voc_annotation(ann_dir, img_dir, cache_name, labels=[]):
 
         cache = {'all_insts': all_insts, 'seen_labels': seen_labels}
         with open(cache_name, 'wb') as handle:
-            pickle.dump(cache, handle, protocol=pickle.HIGHEST_PROTOCOL)    
-                        
+            pickle.dump(cache, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
     return all_insts, seen_labels
